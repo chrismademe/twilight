c71f71bd7955c4e3289d9c0df340cbd2f8eb2693
Chris Galbraith <chris@resknow.co.uk>
2024-06-16 11:56:05 +0100

    Fix mishandling of <!doctype html>

--- index.php
+++ index.php
@@ -24,17 +24,17 @@ $directives->register('for', ForDirective::class);
 $directives->register('html', HtmlDirective::class);
 $directives->register('text', TextDirective::class);
 
-$input = file_get_contents(__DIR__ . '/tests/Unit/input/component/03-component-with-dynamic-attributes.twig');
+$input = file_get_contents(__DIR__ . '/demo-input.twig');
 $tokenizer = new Tokenizer($input);
 
 $tree = new NodeTree($tokenizer->tokenize(), $directives);
 $elements = $tree->create();
 
-// print_r($elements);
+print_r($elements);
 
 $compiler = new Compiler();
 $output = $compiler->compile($elements);
 
-file_put_contents( __DIR__ . '/tests/Unit/output/component/03-component-with-dynamic-attributes.twig', $output );
+echo $output;
 
 echo PHP_EOL . 'Execution time: ' . (number_format(microtime(true) - $timer, 4)) . ' seconds' . PHP_EOL;
